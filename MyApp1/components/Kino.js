c8f10f1efae90f8a90779e6d7415c4c8081c5c1d
Anujin4455 <nijunaanujin0824@gmail.com>
2024-01-15 16:18:14 +0800

    update

---
+++ MyApp1/components/Kino.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Movie from './Movie'
+
+const Kino = () => {
+  return (
+    <View>
+      <Movie />
+    </View>
+  )
+}
+
+export default Kino
+
+const styles = StyleSheet.create({})
---
+++ MyApp1/components/Movie.js
@@ -0,0 +1,46 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Movie = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+      style={styles.img}
+      source={require('../assets/avengers.jpg')}
+       />
+       <Text style={styles.name}>The Avengers</Text>
+       <Text style={styles.decs}>Endgame is a 2019 American superhero film based on the Marvel Comics superhero team the Avengers.</Text>
+    </View>
+  )
+}
+
+export default Movie
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        width: 350,
+        shadowColor: "#000",
+shadowOffset: {
+	width: 0,
+	height: 10,
+},
+shadowOpacity: 0.51,
+shadowRadius: 13.16,
+
+elevation: 20,
+    },
+    img: {
+        width: 300,
+        height: 400,
+    },
+    name: {
+        fontSize: 32,
+        fontWeight: 'bold'
+    },
+    decs:{
+        fontSize: 17,
+
+
+    },
+})
