8b13a97c6c62a50f8917b358191c574f191bf668
Anujin4455 <nijunaanujin0824@gmail.com>
2024-01-15 17:48:28 +0800

    update

--- MyApp1/app/index.js
+++ MyApp1/app/index.js
@@ -5,14 +5,28 @@ import { Link } from 'expo-router';
 const index = () => {
   return (
     <View>
-      <Link href="/about">About</Link>
       {/* ...other links */}
-      <Link href="/movie/Kino">Movies</Link>
-      <Link href="/movie/TicTac">TicTac</Link>
+      <Link href="/movie" style={styles.movie}>Movies</Link>
+      <Link href="/movie/TicTac" style={styles.TicTac}>TicTac</Link>
     </View>
   )
 }
 
 export default index
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  movie: {
+    backgroundColor: '#be95c4',
+    fontSize: 20,
+    borderRadius: 10,
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+  TicTac: {
+    backgroundColor: '#9f86c0',
+    fontSize: 20,
+    borderRadius: 10,
+    justifyContent:'center',
+    textAlign:'center'
+  }
+})
