f6195b608bdf91fb2600a20b8411660cd84e7173
Anujin4455 <nijunaanujin0824@gmail.com>
2024-02-28 18:18:19 +0800

    emoji update

--- DanceFloor/app/emoji.js
+++ DanceFloor/app/emoji.js
@@ -1,4 +1,4 @@
-import { Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Image, ImageBackground, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 import { Link, useLocalSearchParams } from 'expo-router';
 
@@ -6,7 +6,6 @@ const emoji = () => {
 
     const params = useLocalSearchParams()
 
-    console.log('songoltId', params.huniiToo);
 
     const huniiTooLimit = params.huniiToo
     const [songosonEmojinuud, setSetsongosonEmojinuud] = useState([])
@@ -27,48 +26,71 @@ const emoji = () => {
         { id: 14, zurag: require('../assets/DanceFloor/14.png') },
         { id: 15, zurag: require('../assets/DanceFloor/15.png') },
         { id: 16, zurag: require('../assets/DanceFloor/16.png') },
+        { id: 17, zurag: require('../assets/DanceFloor/17.png') },
+        { id: 18, zurag: require('../assets/DanceFloor/18.png') },
+        { id: 19, zurag: require('../assets/DanceFloor/19.jpg') },
+        { id: 20, zurag: require('../assets/DanceFloor/20.jpg') },
+        { id: 21, zurag: require('../assets/DanceFloor/21.jpg') },
+        { id: 22, zurag: require('../assets/DanceFloor/22.jpg') },
+        { id: 23, zurag: require('../assets/DanceFloor/23.jpg') },
     ]
 
 
-    console.log('params.huniiToo', params.huniiToo);
     const emojiSongolt = (emoji) => {
-        console.log(emoji);
-        const shineEmojinuud = [...songosonEmojinuud, emoji.id]
-        console.log('shineEmojinuud', shineEmojinuud.length, shineEmojinuud);
-        if (shineEmojinuud.length <= huniiTooLimit) {
+
+
+        if (songosonEmojinuud.includes(emoji.id)) {
+
+
+            let shineEmojinuud = songosonEmojinuud.filter(e => e !== emoji.id)
+            console.log('shineEmojinuud', songosonEmojinuud, emoji.id, shineEmojinuud);
+
             setSetsongosonEmojinuud(shineEmojinuud)
+        } else {
+            let shineEmojinuud = [...songosonEmojinuud, emoji.id]
+            console.log('shineEmojinuud', shineEmojinuud.length, shineEmojinuud);
+            if (shineEmojinuud.length <= huniiTooLimit) {
+                setSetsongosonEmojinuud(shineEmojinuud)
+            }
         }
 
+
     }
+
+    console.log('songosonEmojinuud', songosonEmojinuud);
     return <View style={styles.emoji}>
+        <ScrollView>
+            <ImageBackground
+                style={styles.bg}
+                source={require('../assets/DanceFloor/emojiWallpaper.png')}  >
 
-        <ImageBackground
-            style={styles.bg}
-            source={require('../assets/DanceFloor/emojiWallpaper.png')}  >
+                <View style={styles.foto}>
 
-            <View style={styles.foto}>
+                    {emojis.map(emoji => {
+                        const isSelected = songosonEmojinuud.find(e => e == emoji.id)
+                        isSelected && console.log('isSelected', isSelected);
+                        return <TouchableOpacity onPress={() => emojiSongolt(emoji)}
 
-                {emojis.map(emoji =>
-                    <TouchableOpacity onPress={() => emojiSongolt(emoji)}
+                            style={{ backgroundColor: isSelected && '#ffcad4' }}
 
-                        style={{ backgroundColor: songosonEmojinuud.includes(emoji.id) ? 'green' : '' }}
+                        >
+                            <Image source={emoji.zurag} style={styles.emojiFto} />
+                        </TouchableOpacity>
+                    })
+                    }
 
-                    >
-                        <Image source={emoji.zurag} style={styles.emojiFto} />
+                </View>
+                <Link href={{
+                    pathname: "/bairlalud",
+                    params: { huniiToo: params.huniiToo, songosonEmojinuud }
+                }} asChild>
+                    <TouchableOpacity style={styles.nextEmj}>
+                        <Text style={styles.nextEmo}>NEXT</Text>
                     </TouchableOpacity>
-                )}
-            </View>
-            <Link href={{
-                pathname: "/bairlalud",
-                params: { huniiToo: params.huniiToo, songosonEmojinuud }
-            }} asChild>
-                <TouchableOpacity style={styles.nextEmj}>
-                    <Text style={styles.nextEmo}>NEXT</Text>
-                </TouchableOpacity>
-            </Link>
+                </Link>
 
-        </ImageBackground>
-
+            </ImageBackground>
+        </ScrollView>
     </View>
 
 }
@@ -86,7 +108,7 @@ const styles = StyleSheet.create({
     emojiFto: {
         width: 100,
         height: 100,
-        backgroundColor: 'lightgray',
+        backgroundColor: 'white',
         opacity: 0.7
     },
     nextEmj: {
@@ -105,6 +127,6 @@ const styles = StyleSheet.create({
         flexWrap: 'wrap',
         justifyContent: 'center',
         alignItems: 'center',
-        gap: 8
+        gap: 8,
     },
 })
